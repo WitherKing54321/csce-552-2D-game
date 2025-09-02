a5648997e3a7f1dee0e95a26144e0673ef32904c
VincentRomero23 <vincentromero278@gmail.com>
2025-09-01 21:13:28 -0400

    animations and textures update

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,19 +1,50 @@
 extends CharacterBody2D
 
-const SPEED := 250.0
-const JUMP_VELOCITY := -300.0   # negative goes UP in Godot 2D
-const GRAVITY := 2000.0
+const SPEED := 150.0
+const JUMP_VELOCITY := -400.0   # negative goes UP in Godot 2D
+const GRAVITY := 1500.0
+
+@onready var sprite = $AnimatedSprite2D
+
+var was_on_floor := false   # to detect landing
+
+func _ready():
+	sprite.play("idle")
 
 func _physics_process(delta):
-	# Horizontal input only; Y is handled by gravity/jump
+	# Horizontal input
 	velocity.x = Input.get_axis("move_left", "move_right") * SPEED
-	#print("Left:", Input.is_action_pressed("move_left"), " | Right:", Input.is_action_pressed("move_right"))
+
+	# Flip sprite depending on direction
+	if velocity.x > 0:
+		sprite.flip_h = true
+	elif velocity.x < 0:
+		sprite.flip_h = false
+
 	# Gravity
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 
-	# Jump
+	# Jump input
 	if is_on_floor() and Input.is_action_just_pressed("move_jump"):
 		velocity.y = JUMP_VELOCITY
 
+	# ---------------------------
+	# Animation state machine
+	# ---------------------------
+	if not is_on_floor():
+		if velocity.y < 0: # going up
+			if sprite.animation != "jump":
+				sprite.play("jump")
+		else: # going down
+			if sprite.animation != "fall":
+				sprite.play("fall")
+	elif velocity.x != 0:
+		sprite.play("run")
+	else:
+		sprite.play("idle")
+
 	move_and_slide()
+
+	# Track landing for next frame
+	was_on_floor = is_on_floor()
