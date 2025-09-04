c7fd74f02b563986e108e853f69b3ddcd06c4c83
VincentRomero23 <vincentromero278@gmail.com>
2025-09-04 17:06:55 -0400

    minor changes

--- player/scripts/GlideState.gd
+++ player/scripts/GlideState.gd
@@ -2,12 +2,17 @@ extends PlayerState
 class_name GlideState
 
 func enter(player):
-	#player.anim.play("bird_appear")
 	player.anim.play("glide")
 
 func physics_update(player, delta):
-	player.velocity.y = min(player.velocity.y, 50) # cap fall speed
+	# Cap vertical velocity
+	player.velocity.y = min(player.velocity.y, 50)
+	var dir = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	player.velocity.x = dir * player.SPEED/1.5
 
+	# Check if glide ends
 	if not Input.is_action_pressed("glide") or player.is_on_floor():
+		#print("TEST_TEST_TEST")
+		#player.velocity.x = 0 # stop sliding horizontally
 		player.anim.play("bird_disappear")
 		player.change_state(IdleState.new())
